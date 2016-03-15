e375f84f814fe227d0331565ecb1b641769dd6cb
Nikolce Ambukovski <lazokin@hotmail.com>
2016-03-15 17:04:36 +1100

    Gotta move it

---
+++ Assets/Scripts/PlayerStateController.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerStateController : MonoBehaviour
+{
+    public enum playerStates
+    {
+        idle = 0,
+        left,
+        right,
+        jump,
+        landing,
+        falling,
+        kill,
+        resurrect
+    }
+
+    public delegate void PlayerStateChangeHandler(PlayerStateController.playerStates newState);
+    public static event PlayerStateChangeHandler StateChange;
+
+
+    void LateUpdate()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        if (horizontal != 0f)
+        {
+            if (horizontal < 0f)
+            {
+                if (StateChange != null)
+                {
+                    StateChange(PlayerStateController.playerStates.left);
+                }
+            }
+            else
+            {
+                if (StateChange != null)
+                {
+                    StateChange(PlayerStateController.playerStates.right);
+                }
+            }
+        }
+        else
+        {
+            if (StateChange != null)
+            {
+                StateChange(PlayerStateController.playerStates.idle);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerStateListener.cs
@@ -0,0 +1,107 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+[RequireComponent(typeof(Animator))]
+public class PlayerStateListener : MonoBehaviour
+{
+    public float playerWalkSpeed = 3f;
+
+    private Animator playerAnimator = null;
+    private PlayerStateController.playerStates currentState = PlayerStateController.playerStates.idle;
+
+    void OnEnable()
+    {
+        PlayerStateController.StateChange += OnStateChange;
+    }
+
+    void OnDisable()
+    {
+        PlayerStateController.StateChange -= OnStateChange;
+    }
+
+    void Start()
+    {
+        playerAnimator = GetComponent<Animator>();
+    }
+
+    void LateUpdate()
+    {
+        OnStateCycle();
+    }
+
+    private void OnStateCycle()
+    {
+        switch (currentState)
+        {
+            case PlayerStateController.playerStates.idle:
+                break;
+
+            case PlayerStateController.playerStates.left:
+                transform.Translate(new Vector3((playerWalkSpeed * -1.0f) * Time.deltaTime, 0.0f, 0.0f));
+                break;
+
+            case PlayerStateController.playerStates.right:
+                transform.Translate(new Vector3((playerWalkSpeed * 1.0f) * Time.deltaTime, 0.0f, 0.0f));
+                break;
+        }
+    }
+
+    private void OnStateChange(PlayerStateController.playerStates newState)
+    {
+        if (newState == currentState)
+        {
+            return;
+        }
+
+        if (StateTransitionAbort(newState))
+        {
+            return;
+        }
+
+        if (!StateTransitionValid(newState))
+        {
+            return;
+        }
+
+        switch (newState)
+        {
+            case PlayerStateController.playerStates.idle:
+                break;
+
+            case PlayerStateController.playerStates.left:
+                break;
+
+            case PlayerStateController.playerStates.right:
+                break;
+        }
+
+        currentState = newState;
+    }
+
+    private bool StateTransitionAbort(PlayerStateController.playerStates newState)
+    {
+        return false;
+    }
+
+    private bool StateTransitionValid(PlayerStateController.playerStates newState)
+    {
+        bool result = false;
+        switch (currentState)
+        {
+            case PlayerStateController.playerStates.idle:
+                result = true;
+                break;
+
+            case PlayerStateController.playerStates.left:
+                result = true;
+                break;
+
+            case PlayerStateController.playerStates.right:
+                result = true;
+                break;
+        }
+        return result;
+    }
+
+}
