9e11a53753ef5712ab00205b6d6d190996e603e0
Nikolce Ambukovski <lazokin@hotmail.com>
2016-03-17 08:31:04 +1100

    Make it move

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -1,10 +1,54 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 
 public class EnemyController : MonoBehaviour
 {
+    public float walkingSpeed = 0.45f;
+    private bool walkingLeft = true;
+
+    public void Start()
+    {
+        walkingLeft = (UnityEngine.Random.Range(0, 2) == 1);
+        updateVisualWalkOrientation();
+    }
+
+    public void Update()
+    {
+        if (walkingLeft)
+        {
+            transform.Translate(new Vector3(-walkingSpeed * Time.deltaTime, 0.0f, 0.0f));
+        }
+        else
+        {
+            transform.Translate(new Vector3(+walkingSpeed * Time.deltaTime, 0.0f, 0.0f));
+        }
+    }
+
     public void switchDirections()
     {
+        walkingLeft = !walkingLeft;
+        updateVisualWalkOrientation();
+    }
 
+    private void updateVisualWalkOrientation()
+    {
+        Vector3 localScale = transform.localScale;
+        if (walkingLeft)
+        {
+            if (localScale.x > 0.0f)
+            {
+                localScale.x = -localScale.x;
+                transform.localScale = localScale;
+            }
+        }
+        else
+        {
+            if (localScale.x < 0.0f)
+            {
+                localScale.x = -localScale.x;
+                transform.localScale = localScale;
+            }
+        }
     }
 }
---
+++ Assets/Scripts/EnemyGuideWatcher.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyGuideWatcher : MonoBehaviour
+{
+
+    public EnemyController enemyController = null;
+
+    public void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Platform")
+        {
+            enemyController.switchDirections();
+        }
+    }
+}
