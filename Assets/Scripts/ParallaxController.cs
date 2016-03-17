a575f24397a5f816f1bc1453c31467987061aef7
Nikolce Ambukovski <lazokin@hotmail.com>
2016-03-17 16:03:50 +1100

    Parallax scrolling

---
+++ Assets/Scripts/ParallaxController.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class ParallaxController : MonoBehaviour
+{
+    public GameObject[] clouds;
+    public GameObject[] nearHills;
+    public GameObject[] farHills;
+
+    public float cloudLayerSpeedMultiplier;
+    public float nearHillsLayerSpeedMultiplier;
+    public float farHillsLayerSpeedMultiplier;
+
+    public Camera myCamera;
+
+    private Vector3 lastCamPos;
+
+    void Start()
+    {
+        lastCamPos = myCamera.transform.position;
+    }
+
+    void Update()
+    {
+        Vector3 currCamPos = myCamera.transform.position;
+        float xPosDiff = lastCamPos.x - currCamPos.x;
+        adjustParallaxPositionsForArray(clouds, cloudLayerSpeedMultiplier, xPosDiff);
+        adjustParallaxPositionsForArray(nearHills, nearHillsLayerSpeedMultiplier, xPosDiff);
+        adjustParallaxPositionsForArray(farHills, farHillsLayerSpeedMultiplier, xPosDiff);
+        lastCamPos = myCamera.transform.position;
+    }
+
+    private void adjustParallaxPositionsForArray(GameObject[] gameObjects, float speedMultiplier, float xPosDiff)
+    {
+        for (int i = 0; i < gameObjects.Length; i++)
+        {
+            Vector3 objPos = gameObjects[i].transform.position;
+            objPos.x += xPosDiff * speedMultiplier;
+            gameObjects[i].transform.position = objPos;
+        }
+    }
+}
