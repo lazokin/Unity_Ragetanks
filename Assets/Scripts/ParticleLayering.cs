f5646c1ce8c0c8fe995914523349d1a7d1612605
Nikolce Ambukovski <lazokin@hotmail.com>
2016-03-18 13:58:06 +1100

    Parallax layer ordering

---
+++ Assets/Scripts/ParticleLayering.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ParticleLayering : MonoBehaviour
+{
+    public string sortLayerString = "";
+
+	void Start () {
+        GetComponent<ParticleSystem>().GetComponent<Renderer>().sortingLayerName = sortLayerString;    
+	}
+}
